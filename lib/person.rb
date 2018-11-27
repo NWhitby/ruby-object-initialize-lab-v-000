aca7bde56f3e631b385f8769949a9ad0247d8714
NaKia Whitby <whitby.nakia@gmail.com>
2018-11-27 19:50:49 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,27 @@
 class Person
+<<<<<<< HEAD
   def initialize(name)
     @name = name
   end
+=======
+  def initialize=(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+>>>>>>> 0e149e6fa5e927764dbcde9e61d93cbe9832957f
 end
